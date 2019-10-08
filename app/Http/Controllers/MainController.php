8bc0ee7bb24125b501943aca14c0f3c2e46e6024
Sebastián Grignoli <grignoli@gmail.com>
2019-10-08 12:24:05 -0400

    Mejor texto de bienvenida

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,13 +9,29 @@ class MainController extends Controller
 {
     public function welcome(Request $request) {
         echo <<<HERE
+<h2>Para "instalar" el comando en la consola:</h2>
+En Bash:
 <pre>
-echo alias publish=\"curl -F file=@- https://publish.ip1.cc\" >> ~/.bashrc
-echo alias publish=\"curl -F file=@- https://publish.ip1.cc\" >> ~/.zsh
+    echo alias publish=\"curl -F file=@- https://publish.ip1.cc\" >> ~/.bashrc
+</pre>
 
-ls | publish
+En Zsh:
+<pre>
+    echo alias publish=\"curl -F file=@- https://publish.ip1.cc\" >> ~/.zsh
+</pre>
+(y reiniciar el shell)
 
-cat file.pdf | publish
+
+<h2>Modo de uso:</h2>
+
+Publicar la salida de la consola:
+<pre>
+    ls | publish
+</pre>
+
+Publicar un archivo:
+<pre>
+    cat file.pdf | publish
 </pre>
 HERE;
     }
